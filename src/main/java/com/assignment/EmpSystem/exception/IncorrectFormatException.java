ca4259d5a49bdcee33ad52c10f2e4ce36af4fe93
keithecq <keithong@ecquaria.com>
2023-02-03 02:03:25 +0800

    Update EmployeeServiceImpl.java

--- src/main/java/com/assignment/EmpSystem/exception/IncorrectFormatException.java
+++ src/main/java/com/assignment/EmpSystem/exception/IncorrectFormatException.java
@@ -8,7 +8,7 @@ import org.springframework.http.HttpStatus;
 @Getter
 @Setter
 @AllArgsConstructor
-public class IncorrectSalaryException extends Exception {
+public class IncorrectFormatException extends Exception {
     private HttpStatus responseStatus;
     private String errorMessage;
 }
--- src/main/java/com/assignment/EmpSystem/service/EmployeeServiceImpl.java
+++ src/main/java/com/assignment/EmpSystem/service/EmployeeServiceImpl.java
@@ -1,26 +1,25 @@
 package com.assignment.EmpSystem.service;
 
 import com.assignment.EmpSystem.dto.response.SubmissionResponse;
-import com.assignment.EmpSystem.exception.IncorrectSalaryException;
+import com.assignment.EmpSystem.exception.IncorrectFormatException;
 import com.assignment.EmpSystem.model.Employee;
 import com.assignment.EmpSystem.repository.EmployeeRepository;
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVParser;
 import org.apache.commons.csv.CSVRecord;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
-
-import org.slf4j.LoggerFactory;
-import org.slf4j.Logger;
-import org.springframework.web.multipart.MultipartFile;
 
 
 @Service
@@ -32,14 +31,14 @@ public class EmployeeServiceImpl implements EmployeeService{
     private static final Logger logger = LoggerFactory.getLogger(EmployeeServiceImpl.class);
     private Integer numOfRows;
 
-    private static boolean checkSalaryFormatMoreThanOneDecimal (CSVRecord csvRecord) throws IncorrectSalaryException {
+    private static boolean checkSalaryFormatMoreThanOneDecimal (CSVRecord csvRecord) throws IncorrectFormatException {
 
         String decimal;
         String number = csvRecord.get("salary");
         try {
             Float salary = Float.parseFloat(number);
             if (salary < 0) {
-                throw new IncorrectSalaryException(HttpStatus.BAD_REQUEST, "Incorrect SalaryFormat (Salary less than 0) Please double check your record at Row : " + csvRecord.getRecordNumber() + csvRecord);
+                throw new IncorrectFormatException(HttpStatus.BAD_REQUEST, "Incorrect SalaryFormat (Salary less than 0) Please double check your record at Row : " + csvRecord.getRecordNumber() + " " + csvRecord.toMap().values());
             }
 
             if (number.contains(".")) {
@@ -47,11 +46,11 @@ public class EmployeeServiceImpl implements EmployeeService{
                 if (decimal.length() == 1) {
                     return false;
                 }
-                throw new IncorrectSalaryException(HttpStatus.BAD_REQUEST, "Incorrect SalaryFormat (More than 1 Decimal Point) Please double check your record at Row : " + csvRecord.getRecordNumber() + csvRecord);
+                throw new IncorrectFormatException(HttpStatus.BAD_REQUEST, "Incorrect SalaryFormat (More than 1 Decimal Point) Please double check your record at Row : " + csvRecord.getRecordNumber() + " " + csvRecord.toMap().values());
             }
-            throw new IncorrectSalaryException(HttpStatus.BAD_REQUEST, "Incorrect SalaryFormat (No Decimals) Please double check your record at Row : " + csvRecord.getRecordNumber() + csvRecord);
+            throw new IncorrectFormatException(HttpStatus.BAD_REQUEST, "Incorrect SalaryFormat (No Decimals) Please double check your record at Row : " + csvRecord.getRecordNumber() + " " + csvRecord.toMap().values());
 
-        } catch (IncorrectSalaryException e) {
+        } catch (IncorrectFormatException e) {
             logger.error(
                     "Uploading CSV file ERROR. Error message: [{}]. HTTP Status: [{}].",
                     e.getErrorMessage(), e.getResponseStatus());
@@ -69,18 +68,32 @@ public class EmployeeServiceImpl implements EmployeeService{
                                                                                 .setSkipHeaderRecord(true)
                                                                                 .build()))
             {
-                Iterable<CSVRecord> records = csvParser.getRecords();
+                List<CSVRecord> csvRecords = csvParser.getRecords();
+                //Validation: Check if Uploaded File is empty
+                if (csvRecords.size() == 0) {
+                    throw new IncorrectFormatException(HttpStatus.BAD_REQUEST, "Uploaded File has no entries");
+                }
+                Iterable<CSVRecord> records = csvRecords;
+
                 List<Employee> empList = new ArrayList<>();
                 for (CSVRecord record : records) {
+
+                    if (record.toMap().values().size() != 4) {
+                        throw new IncorrectFormatException(HttpStatus.BAD_REQUEST, "Incorrect Format (row has missing columns) Please double check your record at Row : " + record.getRecordNumber() + " " + record.toMap().values());
+                    }
+
                     Employee emp = new Employee(
                             record.get("id"),
                             record.get("login"),
                             record.get("name"),
                             Double.parseDouble(record.get("salary"))
                     );
-                    //Salary validation
+                    //Validation: Check if row starts with "#"
+                    if (record.get("id").contains("#")) {
+                        throw new IncorrectFormatException(HttpStatus.BAD_REQUEST, "Incorrect Format (row starts with #) Please double check your record at Row : " + record.getRecordNumber() + " " + record.toMap().values());
+                    }
+                    //Validation: Salary
                     checkSalaryFormatMoreThanOneDecimal(record);
-                    
 
                     //If validation passes add employee to database
                     empList.add(emp);
@@ -89,11 +102,13 @@ public class EmployeeServiceImpl implements EmployeeService{
                 employeeRepository.saveAll(empList);
             } catch (IOException e) {
 
-            } catch (IncorrectSalaryException e) {
-            String errorMessage = e.getErrorMessage();
-            responseEntity = ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new SubmissionResponse(errorMessage));
-            return responseEntity;
-        }
+//            } catch() {
+
+            } catch (IncorrectFormatException e) {
+                String errorMessage = e.getErrorMessage();
+                responseEntity = ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new SubmissionResponse(errorMessage));
+                return responseEntity;
+            }
 
 
         String message = "Succesfully submitted CSV file. Rows Updated: " + this.numOfRows;
